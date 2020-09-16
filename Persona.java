311583dd4af4607b24609b4e17dd2ff0f704c2fa
AgustinEscobar <agustinescobar700@gmail.com>
2020-09-16 20:26:29 -0300

    commit inicial

---
+++ Persona.java
@@ -0,0 +1,17 @@
+package Proyecto1_TDP;
+
+public class Persona {
+	private String nombre;
+	
+	public Persona(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String get_nombre() {
+		return this.nombre;
+	}
+	
+	public void set_nombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
---
+++ Tester.java
@@ -0,0 +1,8 @@
+package Proyecto1_TDP;
+
+public class Tester {
+	public static void main (String [] args) {
+		Persona p = new Persona("Agustin ");
+		System.out.println("Nombre: "+p.get_nombre());
+	}
+}
