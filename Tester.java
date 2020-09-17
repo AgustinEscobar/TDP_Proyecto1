2ca9781a5b3d6c77a616b5a8fd757aff832d28e3
AgustinEscobar <agustinescobar700@gmail.com>
2020-09-16 21:25:27 -0300

    Segunda modificacion

--- Tester.java
+++ Tester.java
@@ -2,7 +2,7 @@ package Proyecto1_TDP;
 
 public class Tester {
 	public static void main (String [] args) {
-		Persona p = new Persona("Agustin ");
+		Persona p = new Persona("Oscar ");
 		System.out.println("Nombre: "+p.get_nombre());
 	}
 }
